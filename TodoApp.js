a21367147852c111f38cb3aace24da5442e6b320
Landon E. <161777253+lellis9@users.noreply.github.com>
2024-08-03 17:42:32 -0400

    Add files via upload

---
+++ TodoApp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>To Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} removeTodo={removeTodo} />
+    </div>
+  );
+}
+
+export default TodoApp;
